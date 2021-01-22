310a1eedd3a121b1f454809dac313e8248c545f3
Geoff Thompson <thompsgr@avaion.com>
2021-01-22 00:00:06 -0600

    getting somewhere

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 const fs = require('fs');
 
-const showdown = require('showdown');
-showdown.setOption('tables', true);
-const converter = new showdown.Converter();
 
 function parse(file) {
     let [name, ext] = file.split('.');    
@@ -18,7 +15,7 @@ function isMarkdown(file) {
 }
 
 // set directories from args if provided
-const indir = process.argv[2] || 'source/';
+const indir = process.argv[2] || 'doc/';
 console.log(`markdown directory (input): ${indir}`);
 const outdir = process.argv[3] || 'public/';
 console.log(`html directory (output): ${outdir}`);
@@ -28,17 +25,20 @@ let data = '', html = '', toc = '';
 const files = fs.readdirSync(indir).map(parse).filter(isMarkdown);
 process.stdout.write(`processing ${files.length} files: `);
 
+
 files.forEach(function(file) {
     process.stdout.write(`.`);    
-    toc += `<li><a href='${file.name}.html'>${file.name}</a></li>`;
+    toc += `\t\t<li><a href='${file.name}.html'>${file.name}</a></li>\n`;
     data = fs.readFileSync(`${indir}/${file.name}.${file.ext}`, 'utf8');
-    html = converter.makeHtml(data);
-    fs.writeFileSync(`${outdir}/${file.name}.html`, html);
+    let htmlText = data
+        .replace(/^## (.*$)/gim, '<h2>$1</h2>')    
+	.replace(/^# (.*$)/gim, '<h1>$1</h1>');
+    fs.writeFileSync(`${outdir}/${file.name}.html`, htmlText);
 });
 process.stdout.write('done\n');
 
 // write toc
-fs.writeFileSync(`${outdir}/toc.html`, `<nav><ol>${toc}</ol></nav>`);
+fs.writeFileSync(`${outdir}/toc.html`, `<h1>Table of Contents</h1>\n<nav>\n\t<ol>\n${toc}\t</ol>\n</nav>\n`);
 console.log(`table of contents written`);
 
 console.log(`processing complete.`);
